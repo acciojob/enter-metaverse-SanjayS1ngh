01783af085893c86cb4cdab73997e181543609a9
Sanjay Singh <114378504+SanjayS1ngh@users.noreply.github.com>
2026-08-07 09:43:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let btn=document.getElementById("enterBtn");
 let sts=document.getElementById("status");
 
 btn.onclick=function(){
-	sts.outerHTML="<h1>Entered Metaverse</h1>"
+	sts.outerHTML="<h1 id="status">Entered Metaverse</h1>"
 }
